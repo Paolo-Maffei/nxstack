fabeb42cbf19cac8a03c8418f6334e0e5e154268
zhouq3132@gmail.com <zhouq3132@gmail.com@17f6a954-993f-ed27-2e75-2d84de3cd8ff>
2012-05-16 09:32:44 +0000

    next is to modify rf_ram_set()

--- Examples/x_meter_gateway/.deps/deps/Platform/xmeter/rf.d
+++ Examples/x_meter_gateway/.deps/deps/Platform/xmeter/rf.d
@@ -1,4 +1,15 @@
-.objs/build/root/x_meter_gateway/../../Platform/xmeter/rf.o: ../../Platform/xmeter/rf.c ../../FreeRTOS/Source/include/FreeRTOS.h \
+.objs/build/root/x_meter_gateway/../../Platform/xmeter/rf.o: ../../Platform/xmeter/rf.c ../../Platform/xmeter/cmsis/stm32f10x.h \
+ ../../Platform/xmeter/cmsis/core_cm3.h \
+ ../../Platform/xmeter/cmsis/system_stm32f10x.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_conf.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_exti.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_gpio.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_rcc.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_spi.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_tim.h \
+ ../../Platform/xmeter/stdplib/inc/stm32f10x_usart.h \
+ ../../Platform/xmeter/stdplib/inc/misc.h \
+ ../../FreeRTOS/Source/include/FreeRTOS.h \
  ../../FreeRTOS/Source/include/projdefs.h FreeRTOSConfig.h \
  ../../FreeRTOS/Source/include/portable.h \
  ../../Platform/xmeter/portmacro.h ../../FreeRTOS/Source/include/queue.h \
@@ -13,5 +24,5 @@
  ../../Common/include/rf_802_15_4.h ../../Platform/xmeter/include/rf.h \
  ../../Common/include/neighbor_routing_table.h \
  ../../Common/include/debug.h ../../Platform/xmeter/include/progmem.h \
- ../../Common/include/mac.h ../../Platform/xmeter/include/gpio.h \
- ../../Common/include/rf_802_15_4.h
+ ../../Common/include/mac.h ../../Platform/xmeter/include/cc2520hal.h \
+ ../../Platform/xmeter/include/gpio.h ../../Common/include/rf_802_15_4.h
