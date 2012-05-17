6724b8d84508ab5b4b88490b39b9c0d6f5d057cd
zhouq3132@gmail.com <zhouq3132@gmail.com@17f6a954-993f-ed27-2e75-2d84de3cd8ff>
2012-05-17 13:10:06 +0000

    2012-05-17 last commit,the next is to flash the exe file to board

--- trunk/Examples/x_meter_gateway/FreeRTOSConfig.h
+++ trunk/Examples/x_meter_gateway/FreeRTOSConfig.h
@@ -99,10 +99,10 @@ to exclude the API function. */
 
 #define INCLUDE_vTaskPrioritySet		0
 #define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete				0
-#define INCLUDE_vTaskCleanUpResources	0
+#define INCLUDE_vTaskDelete			0
+#define INCLUDE_vTaskCleanUpResources		0
 #define INCLUDE_vTaskSuspend			0
-#define INCLUDE_vTaskDelayUntil			0
-#define INCLUDE_vTaskDelay				1
+#define INCLUDE_vTaskDelayUntil			1
+#define INCLUDE_vTaskDelay			1
 
 #endif /* FREERTOS_CONFIG_H */
