7d165fed333c5a64f91c4e240004974226be0a17
zhouq3132@gmail.com <zhouq3132@gmail.com@17f6a954-993f-ed27-2e75-2d84de3cd8ff>
2012-05-25 09:29:27 +0000

    next to modify pendsv_isr sp related code

--- trunk/Examples/x_meter_gateway/FreeRTOSConfig.h
+++ trunk/Examples/x_meter_gateway/FreeRTOSConfig.h
@@ -81,25 +81,25 @@
 #define configUSE_PREEMPTION		1
 #define configUSE_TICK_HOOK			0
 #define configCPU_CLOCK_HZ			( ( unsigned portLONG ) 72000000 )
-#define configTICK_RATE_HZ			( ( portTickType ) 1000 )
+#define configTICK_RATE_HZ			( ( portTickType ) 100 )
 #define configMINIMAL_STACK_SIZE	( ( unsigned portSHORT ) 50 )
-#define configMAXIMUM_STACK_SIZE        ( ( unsigned portSHORT ) 400)
-#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 12000 ) )
+#define configMAXIMUM_STACK_SIZE        ( ( unsigned portSHORT ) 300)
+#define configTOTAL_HEAP_SIZE		( ( size_t ) ( 8000 ) )
 #define configMAX_TASK_NAME_LEN		( 8 )
 #define configUSE_TRACE_FACILITY	0
 #define configUSE_16_BIT_TICKS		0
 #define configIDLE_SHOULD_YIELD		0
 #define configUSE_CO_ROUTINES 		1
 
-#define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 2 )
-#define configMAX_CO_ROUTINE_PRIORITIES ( 2 )
+#define configMAX_PRIORITIES		( ( unsigned portBASE_TYPE ) 8 )
+#define configMAX_CO_ROUTINE_PRIORITIES ( 1 )
 
 /* Set the following definitions to 1 to include the API function, or zero
 to exclude the API function. */
 
 #define INCLUDE_vTaskPrioritySet		0
 #define INCLUDE_uxTaskPriorityGet		0
-#define INCLUDE_vTaskDelete			0
+#define INCLUDE_vTaskDelete			1
 #define INCLUDE_vTaskCleanUpResources		0
 #define INCLUDE_vTaskSuspend			0
 #define INCLUDE_vTaskDelayUntil			1
